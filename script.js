8051653f47b7f13564d83bab155c0b0933a8ca12
Christian Ramirez <christian.r0602@gmail.com>
2022-07-09 21:22:56 -0500

    Push front

---
+++ script.js
@@ -0,0 +1,10 @@
+// Given an array and an additional value, insert this value at the beginning of the array. You may use .push(), you are able do this without it though!
+
+// function pushFront(arr, val){
+//     const original = [val, ...arr ]
+  
+//   return original
+// }
+
+
+// console.log(pushFront([1,2,3,4], 8))
