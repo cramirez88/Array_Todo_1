ca1c8a19d196d4894fc38eac15e386203762bc83
Christian Ramirez <christian.r0602@gmail.com>
2022-07-09 21:36:55 -0500

    popFront

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
 
 
 // console.log(pushFront([1,2,3,4], 8))
+
+// Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it. You may use .pop(), you are able do this without it though!
+
+// Examples:
+
+// popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
+// popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
+
+// function popFront(arr){
+//   const [, ...rest] = arr
+//   return rest
+// }
+
+// console.log(popFront([1,4,6]))
